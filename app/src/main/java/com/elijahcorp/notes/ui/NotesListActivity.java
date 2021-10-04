b49b37a89e6e24edba38f825fcd224616be28ee7
ilya <ilja.kasatckin@yandex.ru>
2021-10-05 01:58:27 +0300

    Notes:homework1:RecycleView: added methods for display toolbar with menu

--- app/src/main/java/com/elijahcorp/notes/ui/NotesListActivity.java
+++ app/src/main/java/com/elijahcorp/notes/ui/NotesListActivity.java
@@ -1,16 +1,39 @@
 package com.elijahcorp.notes.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.elijahcorp.notes.R;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class NotesListActivity extends AppCompatActivity {
+    private MaterialToolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        topAppBar = findViewById(R.id.top_app_bar);
+        setSupportActionBar(topAppBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.notes_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_note_menu) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
