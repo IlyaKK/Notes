d16c8ed4cc31d9696b66e9a2396a632c4a8042c9
ilya <ilja.kasatckin@yandex.ru>
2021-11-02 21:12:47 +0300

    Notes:FromJavaToKotlin: create kotlin classes from NoteEditFragment and NotesListFragment.kt

--- app/src/main/java/com/elijahcorp/notes/ui/NoteEditFragment.kt
+++ app/src/main/java/com/elijahcorp/notes/ui/NoteEditFragment.kt
@@ -1,222 +1,198 @@
-package com.elijahcorp.notes.ui;
+package com.elijahcorp.notes.ui
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.elijahcorp.notes.R
+import com.elijahcorp.notes.domain.Note
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.atomic.AtomicReference
+import java.util.regex.Pattern
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+class NoteEditFragment : Fragment() {
+    private lateinit var titleEditText: TextInputEditText
+    private lateinit var descriptionEditText: TextInputEditText
+    private lateinit var note: Note
+    private var isEmptyNote = false
+    private var controller: Controller? = null
+    private var topAppBarListener: TopAppBarListener? = null
 
-import com.elijahcorp.notes.R;
-import com.elijahcorp.notes.domain.Note;
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.timepicker.MaterialTimePicker;
-import com.google.android.material.timepicker.TimeFormat;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.atomic.AtomicReference;
-
-public class NoteEditFragment extends Fragment {
-    private TextInputEditText titleEditText;
-    private TextInputEditText descriptionEditText;
-    public static final String INFO_NOTE_KEY = "info_note_key";
-    public static final String CHANGE_NOTE_KEY = "change_note_key";
-    public final static String NOTE_EDIT_FRAGMENT = "NOTE_EDIT_FRAGMENT";
-    private Note note;
-    private boolean isEmptyNote = false;
-    private Controller controller;
-    private TopAppBarListener topAppBarListener;
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        if (titleEditText.getText() != null && descriptionEditText.getText() != null) {
-            note.setTitle(titleEditText.getText().toString());
-            note.setDescription(descriptionEditText.getText().toString());
+    override fun onSaveInstanceState(outState: Bundle) {
+        if (titleEditText.text != null && descriptionEditText.text != null) {
+            note.title = titleEditText.text.toString()
+            note.description = descriptionEditText.text.toString()
             if (isEmptyNote) {
-                note.setTimeCreate("");
+                note.timeCreate = ""
             }
-            outState.putParcelable(CHANGE_NOTE_KEY, note);
+            outState.putParcelable(CHANGE_NOTE_KEY, note)
         }
-        super.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState)
     }
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof Controller) {
-            controller = (Controller) context;
-            topAppBarListener = (TopAppBarListener) context;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Controller) {
+            controller = context
+            topAppBarListener = context as TopAppBarListener
         } else {
-            throw new IllegalStateException("Activity doesn't have impl NoteEditFragment.Controller or NoteEditFragment.TopAppBarListener interface");
+            throw IllegalStateException("Activity doesn't have impl NoteEditFragment.Controller or NoteEditFragment.TopAppBarListener interface")
         }
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_note_edit, container, false)
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        initViews(view);
-        initialiseTopAppBar();
-        getNoteFromNotesList();
-        fillEditTexts();
-        initialiseBackSpaceCallBack();
-        changerOrientationScreen(savedInstanceState);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initViews(view)
+        initialiseTopAppBar()
+        noteFromNotesList
+        fillEditTexts()
+        initialiseBackSpaceCallBack()
+        changerOrientationScreen(savedInstanceState)
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        menu.clear();
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
         if (!isEmptyNote) {
-            inflater.inflate(R.menu.notes_edit_menu, menu);
+            inflater.inflate(R.menu.notes_edit_menu, menu)
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.change_create_time) {
-            initialiseChangeCreateTimeDataPicker();
-            return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.change_create_time) {
+            initialiseChangeCreateTimeDataPicker()
+            return true
         }
-        return false;
+        return false
     }
 
-    private void initialiseChangeCreateTimeDataPicker() {
-        AtomicReference<String> data = new AtomicReference<>();
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+    private fun initialiseChangeCreateTimeDataPicker() {
+        val data = AtomicReference<String>()
+        @SuppressLint("SimpleDateFormat") val formatter = SimpleDateFormat("dd.MM.yyyy HH:mm")
         try {
-            Date date = formatter.parse(note.getTimeCreate());
+            val date = formatter.parse(note.timeCreate)
             if (date != null) {
-                MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                val datePicker = MaterialDatePicker.Builder.datePicker()
                         .setTitleText(R.string.select_data_create)
-                        .setSelection(date.getTime())
-                        .build();
-
-                MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+                        .setSelection(date.time)
+                        .build()
+                val timePicker = MaterialTimePicker.Builder()
                         .setTimeFormat(TimeFormat.CLOCK_24H)
                         .setTitleText(R.string.select_time_created)
-                        .setHour(Integer.parseInt(note.getTimeCreate().split(" ", 2)[1].split(":", 2)[0]))
-                        .setMinute(Integer.parseInt(note.getTimeCreate().split(" ", 2)[1].split(":", 2)[1]))
-                        .build();
-
-                datePicker.show(getParentFragmentManager(), "data picker");
-
-                datePicker.addOnPositiveButtonClickListener(selection -> {
-                    @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.setTimeInMillis(selection);
-                    timePicker.show(getParentFragmentManager(), "time picker");
-                    data.set(format.format(calendar.getTime()));
-                });
-
-                timePicker.addOnPositiveButtonClickListener((l) -> {
-                    @SuppressLint("DefaultLocale") String time = String.format("%02d:%02d", timePicker.getHour(), timePicker.getMinute());
-                    data.set(data.get() + " " + time);
-                    note.setTimeCreate(data.get());
-                });
+                        .setHour(Pattern.compile(" ").split(note.timeCreate, 2)[1].split(":")[0].toInt())
+                        .setMinute(Pattern.compile(" ").split(note.timeCreate, 2)[1].split(":")[1].toInt())
+                        .build()
+                datePicker.show(parentFragmentManager, "data picker")
+                datePicker.addOnPositiveButtonClickListener { selection: Long? ->
+                    @SuppressLint("SimpleDateFormat") val format = SimpleDateFormat("dd.MM.yyyy")
+                    val calendar = Calendar.getInstance()
+                    calendar.timeInMillis = selection!!
+                    timePicker.show(parentFragmentManager, "time picker")
+                    data.set(format.format(calendar.time))
+                }
+                timePicker.addOnPositiveButtonClickListener {
+                    @SuppressLint("DefaultLocale") val time = String.format("%02d:%02d", timePicker.hour, timePicker.minute)
+                    data.set(data.get().toString() + " " + time)
+                    note.timeCreate = data.get()
+                }
             }
-
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (e: ParseException) {
+            e.printStackTrace()
         }
     }
 
-    private void initViews(View view) {
-        titleEditText = view.findViewById(R.id.title_edit_text);
-        descriptionEditText = view.findViewById(R.id.description_edit_text);
+    private fun initViews(view: View) {
+        titleEditText = view.findViewById(R.id.title_edit_text)
+        descriptionEditText = view.findViewById(R.id.description_edit_text)
     }
 
-    private void initialiseTopAppBar() {
-        topAppBarListener.changeTopAppBar(NOTE_EDIT_FRAGMENT);
+    private fun initialiseTopAppBar() {
+        topAppBarListener!!.changeTopAppBar(NOTE_EDIT_FRAGMENT)
     }
 
-    private void getNoteFromNotesList() {
-        Bundle args = getArguments();
-        if (args != null) {
-            if (args.containsKey(CHANGE_NOTE_KEY)) {
-                note = args.getParcelable(CHANGE_NOTE_KEY);
-            } else {
-                note = args.getParcelable(INFO_NOTE_KEY);
+    private val noteFromNotesList: Unit
+        get() {
+            val args = arguments
+            if (args != null) {
+                note = if (args.containsKey(CHANGE_NOTE_KEY)) {
+                    args.getParcelable(CHANGE_NOTE_KEY)!!
+                } else {
+                    args.getParcelable(INFO_NOTE_KEY)!!
+                }
             }
         }
-    }
 
-    private void fillEditTexts() {
-        if (note.getTitle().isEmpty() && note.getDescription().isEmpty()) {
-            isEmptyNote = true;
+    private fun fillEditTexts() {
+        if (note.title.isEmpty() && note.description.isEmpty()) {
+            isEmptyNote = true
         }
-        titleEditText.setText(note.getTitle());
-        descriptionEditText.setText(note.getDescription());
+        titleEditText.setText(note.title)
+        descriptionEditText.setText(note.description)
     }
 
-    private void initialiseBackSpaceCallBack() {
-        controller.initialiseNavigationIconBack();
+    private fun initialiseBackSpaceCallBack() {
+        controller!!.initialiseNavigationIconBack()
     }
 
-    public void saveChangeNote() {
-        if (titleEditText.getText() != null && descriptionEditText.getText() != null) {
-            note.setTitle(titleEditText.getText().toString());
-            note.setDescription(descriptionEditText.getText().toString());
+    fun saveChangeNote() {
+        if (titleEditText.text != null && descriptionEditText.text != null) {
+            note.title = titleEditText.text.toString()
+            note.description = descriptionEditText.text.toString()
             if (isEmptyNote) {
-                note.setTimeCreate("");
+                note.timeCreate = ""
             }
-            Bundle bundle = new Bundle();
-            bundle.putParcelable(CHANGE_NOTE_KEY, note);
-            setArguments(bundle);
+            val bundle = Bundle()
+            bundle.putParcelable(CHANGE_NOTE_KEY, note)
+            arguments = bundle
         }
     }
 
-    public static NoteEditFragment newInstance(Note note) {
-        NoteEditFragment noteEditFragment = new NoteEditFragment();
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(INFO_NOTE_KEY, note);
-        noteEditFragment.setArguments(bundle);
-        return noteEditFragment;
-    }
-
-    private void changerOrientationScreen(Bundle savedInstanceState) {
+    private fun changerOrientationScreen(savedInstanceState: Bundle?) {
         if (savedInstanceState != null) {
-            Note note = savedInstanceState.getParcelable(CHANGE_NOTE_KEY);
-            controller.displayNoteEdit(note);
-            controller.deleteOldFragment(this);
+            val note: Note? = savedInstanceState.getParcelable(CHANGE_NOTE_KEY)
+            controller!!.displayNoteEdit(note)
+            controller!!.deleteOldFragment(this)
         }
     }
 
-    interface Controller {
-
-        void initialiseNavigationIconBack();
-
-        void displayNoteEdit(Note note);
-
-        void deleteOldFragment(Fragment noteEditFragment);
+    internal interface Controller {
+        fun initialiseNavigationIconBack()
+        fun displayNoteEdit(note: Note?)
+        fun deleteOldFragment(noteEditFragment: Fragment?)
     }
 
-    interface TopAppBarListener {
-
-        void changeTopAppBar(String nameFragment);
+    internal interface TopAppBarListener {
+        fun changeTopAppBar(nameFragment: String?)
     }
 
-    @Override
-    public void onDestroy() {
-        controller = null;
-        topAppBarListener = null;
-        super.onDestroy();
+    override fun onDestroy() {
+        controller = null
+        topAppBarListener = null
+        super.onDestroy()
+    }
+
+    companion object {
+        const val INFO_NOTE_KEY = "info_note_key"
+        const val CHANGE_NOTE_KEY = "change_note_key"
+        const val NOTE_EDIT_FRAGMENT = "NOTE_EDIT_FRAGMENT"
+        @JvmStatic
+        fun newInstance(note: Note?): NoteEditFragment {
+            val noteEditFragment = NoteEditFragment()
+            val bundle = Bundle()
+            bundle.putParcelable(INFO_NOTE_KEY, note)
+            noteEditFragment.arguments = bundle
+            return noteEditFragment
+        }
     }
 }
-
 
--- app/src/main/java/com/elijahcorp/notes/ui/NotesListFragment.kt
+++ app/src/main/java/com/elijahcorp/notes/ui/NotesListFragment.kt
@@ -1,179 +1,152 @@
-package com.elijahcorp.notes.ui;
+package com.elijahcorp.notes.ui
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.content.Context
+import android.graphics.Canvas
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.*
+import com.elijahcorp.notes.R
+import com.elijahcorp.notes.domain.Note
+import com.elijahcorp.notes.domain.NotesRepo
+import com.elijahcorp.notes.impl.NotesRepoImpl
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+class NotesListFragment : Fragment() {
+    private lateinit var notesRecycleView: RecyclerView
+    private val notesAdapter = NotesAdapter()
+    private val notesRepo: NotesRepo = NotesRepoImpl()
+    private lateinit var swipeDeleteCallback: ItemTouchHelper.SimpleCallback
+    private var controller: Controller? = null
+    private var topAppBarListener: TopAppBarListener? = null
 
-import com.elijahcorp.notes.R;
-import com.elijahcorp.notes.domain.Note;
-import com.elijahcorp.notes.domain.NotesRepo;
-import com.elijahcorp.notes.impl.NotesRepoImpl;
-
-public class NotesListFragment extends Fragment {
-
-    private RecyclerView notesRecycleView;
-    private final NotesAdapter notesAdapter = new NotesAdapter();
-    private final NotesRepo notesRepo = new NotesRepoImpl();
-    private ItemTouchHelper.SimpleCallback swipeDeleteCallback;
-    private Controller controller;
-    public static final String NOTES_LIST_FRAGMENT = "NOTES_LIST_FRAGMENT";
-    private TopAppBarListener topAppBarListener;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof Controller) {
-            controller = (Controller) context;
-            topAppBarListener = (TopAppBarListener) context;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Controller) {
+            controller = context
+            topAppBarListener = context as TopAppBarListener
         } else {
-            throw new IllegalStateException("Activity doesn't have impl NotesListFragment.Controller or NotesListFragment.TopAppBarListener interface");
+            throw IllegalStateException("Activity doesn't have impl NotesListFragment.Controller or NotesListFragment.TopAppBarListener interface")
         }
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_notes_list, container, false)
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        initialiseViews(view);
-        initialiseTopAppBar();
-        fillDataFromFile();
-        initialiseDeleteNoteSwipe();
-        initialiseNotesRecycleView();
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initialiseViews(view)
+        initialiseTopAppBar()
+        fillDataFromFile()
+        initialiseDeleteNoteSwipe()
+        initialiseNotesRecycleView()
     }
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        menu.clear();
-        inflater.inflate(R.menu.notes_list_menu, menu);
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.notes_list_menu, menu)
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.add_note_menu) {
-            initialiseAddNoteToNotesList();
-            return true;
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_note_menu) {
+            initialiseAddNoteToNotesList()
+            return true
         }
-        return false;
+        return false
     }
 
-    private void initialiseAddNoteToNotesList() {
-        Note note = new Note("", "");
-        controller.displayNoteEdit(note);
+    private fun initialiseAddNoteToNotesList() {
+        val note = Note("", "")
+        controller!!.displayNoteEdit(note)
     }
 
-    private void initialiseViews(View view) {
-        notesRecycleView = view.findViewById(R.id.notes_recycle_view);
+    private fun initialiseViews(view: View) {
+        notesRecycleView = view.findViewById(R.id.notes_recycle_view)
     }
 
-    public void initialiseTopAppBar() {
-        topAppBarListener.changeTopAppBar(NOTES_LIST_FRAGMENT);
+    fun initialiseTopAppBar() {
+        topAppBarListener!!.changeTopAppBar(NOTES_LIST_FRAGMENT)
     }
 
-    private void fillDataFromFile() {
-        notesRepo.readNotes(requireContext());
+    private fun fillDataFromFile() {
+        notesRepo.readNotes(requireContext())
     }
 
-    private void initialiseDeleteNoteSwipe() {
-        swipeDeleteCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return true;
+    private fun initialiseDeleteNoteSwipe() {
+        swipeDeleteCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return true
             }
 
-            @Override
-            public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
                 if (viewHolder != null) {
-                    getDefaultUIUtil().onSelected(((NoteVh) viewHolder).getForegroundNoteCardView());
+                    getDefaultUIUtil().onSelected((viewHolder as NoteVh).foregroundNoteCardView)
                 }
             }
 
-            @Override
-            public void onChildDrawOver(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
-                                        RecyclerView.ViewHolder viewHolder, float dX, float dY,
-                                        int actionState, boolean isCurrentlyActive) {
-                getDefaultUIUtil().onDrawOver(c, recyclerView, ((NoteVh) viewHolder).getForegroundNoteCardView(), dX, dY,
-                        actionState, isCurrentlyActive);
+            override fun onChildDrawOver(c: Canvas, recyclerView: RecyclerView,
+                                         viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float,
+                                         actionState: Int, isCurrentlyActive: Boolean) {
+                getDefaultUIUtil().onDrawOver(c, recyclerView, (viewHolder as NoteVh).foregroundNoteCardView, dX, dY,
+                        actionState, isCurrentlyActive)
             }
 
-            @Override
-            public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
-                getDefaultUIUtil().clearView(((NoteVh) viewHolder).getForegroundNoteCardView());
+            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                getDefaultUIUtil().clearView((viewHolder as NoteVh).foregroundNoteCardView)
             }
 
-            @Override
-            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
-                                    @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
-                                    int actionState, boolean isCurrentlyActive) {
-                getDefaultUIUtil().onDraw(c, recyclerView, ((NoteVh) viewHolder).getForegroundNoteCardView(), dX, dY,
-                        actionState, isCurrentlyActive);
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView,
+                                     viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float,
+                                     actionState: Int, isCurrentlyActive: Boolean) {
+                getDefaultUIUtil().onDraw(c, recyclerView, (viewHolder as NoteVh).foregroundNoteCardView, dX, dY,
+                        actionState, isCurrentlyActive)
             }
 
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                notesRepo.deleteNote(requireContext(), viewHolder.getAdapterPosition());
-                notesAdapter.setData(notesRepo.getNotes());
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                notesRepo.deleteNote(requireContext(), viewHolder.adapterPosition)
+                notesAdapter.setData(notesRepo.notes)
             }
-        };
-    }
-
-    private void initialiseNotesRecycleView() {
-        notesRecycleView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        notesRecycleView.setItemAnimator(new DefaultItemAnimator());
-        notesRecycleView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
-        notesRecycleView.setAdapter(notesAdapter);
-        new ItemTouchHelper(swipeDeleteCallback).attachToRecyclerView(notesRecycleView);
-        notesAdapter.setData(notesRepo.getNotes());
-        notesAdapter.setOnCardClickListener(this::onCardClickListener);
-    }
-
-    private void onCardClickListener(Note note) {
-        controller.displayNoteEdit(note);
-    }
-
-    public void setNoteChange(Note note) {
-        if (note.getTimeCreate().isEmpty()) {
-            notesRepo.createNote(requireContext(), note);
-        } else {
-            notesRepo.updateNote(requireContext(), note);
         }
-        notesAdapter.setData(notesRepo.getNotes());
     }
 
-    interface Controller {
-
-        void displayNoteEdit(Note note);
-
+    private fun initialiseNotesRecycleView() {
+        notesRecycleView.layoutManager = LinearLayoutManager(requireContext())
+        notesRecycleView.itemAnimator = DefaultItemAnimator()
+        notesRecycleView.addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        notesRecycleView.adapter = notesAdapter
+        ItemTouchHelper(swipeDeleteCallback).attachToRecyclerView(notesRecycleView)
+        notesAdapter.setData(notesRepo.notes)
+        notesAdapter.setOnCardClickListener { note: Note -> onCardClickListener(note) }
     }
 
-    interface TopAppBarListener {
-
-        void changeTopAppBar(String nameFragment);
+    private fun onCardClickListener(note: Note) {
+        controller!!.displayNoteEdit(note)
     }
 
-    @Override
-    public void onDestroy() {
-        controller = null;
-        topAppBarListener = null;
-        super.onDestroy();
+    fun setNoteChange(note: Note) {
+        if (note.timeCreate.isEmpty()) {
+            notesRepo.createNote(requireContext(), note)
+        } else {
+            notesRepo.updateNote(requireContext(), note)
+        }
+        notesAdapter.setData(notesRepo.notes)
+    }
+
+    internal interface Controller {
+        fun displayNoteEdit(note: Note?)
+    }
+
+    internal interface TopAppBarListener {
+        fun changeTopAppBar(nameFragment: String?)
+    }
+
+    override fun onDestroy() {
+        controller = null
+        topAppBarListener = null
+        super.onDestroy()
+    }
+
+    companion object {
+        const val NOTES_LIST_FRAGMENT = "NOTES_LIST_FRAGMENT"
     }
 }
